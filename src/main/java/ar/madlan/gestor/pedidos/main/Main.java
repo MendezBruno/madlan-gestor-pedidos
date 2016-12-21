7c53330f886b26abc74b4ffcf6f1d0a9209ab3fb
eayzenberg <eayzenberg>
2016-12-21 13:41:48 -0300

    Saco ventaja del mixin

--- src/main/java/ar/madlan/gestor/pedidos/main/Main.java
+++ src/main/java/ar/madlan/gestor/pedidos/main/Main.java
@@ -2,9 +2,6 @@ package ar.madlan.gestor.pedidos.main;
 
 import ar.madlan.gestor.pedidos.vista.PedidosController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -15,10 +12,8 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader fxmlLoader = new FXMLLoader();
-		fxmlLoader.setLocation(PedidosController.class.getResource("pedidos.fxml"));
-		fxmlLoader.setController(new PedidosController());
-		primaryStage.setScene(new Scene((Parent) fxmlLoader.load()));
+		PedidosController controller = new PedidosController();
+		controller.cargar(primaryStage);
 		primaryStage.show();
 	}
 
